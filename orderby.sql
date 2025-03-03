a8401699ca2ae58649a5626d015c8a47ebd93832
Kirat Anand <167013352+kirat-anand-68@users.noreply.github.com>
2025-03-03 17:49:27 +0530

    Add files via upload

---
+++ orderby.sql
@@ -0,0 +1,12 @@
+
+select * from [dbo].[Employees]
+
+order by Salary
+
+select * from [dbo].[Employees]
+
+order by Salary desc
+
+select * from [dbo].[Employees]
+
+order by FirstName asc,Salary desc
